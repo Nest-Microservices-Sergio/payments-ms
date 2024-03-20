533b826ddaf16d23dfb4add59db9d9033dffa89c
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-20 08:54:42 -0700

    sesión de pago

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -5,4 +5,30 @@ import Stripe from 'stripe';
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+
+  async createPaymentSession() {
+    const session = await this.stripe.checkout.sessions.create({
+      //Colocar aquí el ID de mi orden
+      payment_intent_data: {
+        metadata: {},
+      },
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'T-Shirt',
+            },
+            unit_amount: 2000,
+          },
+          quantity: 2,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3003/payments/success',
+      cancel_url: 'http://localhost:3003/payments/cancelled',
+    });
+
+    return session;
+  }
 }
